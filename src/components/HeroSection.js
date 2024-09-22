0ee45bd34d1cfcde159a97bc8df3b4a3dc80930f
Javier Peral <javiperalb@gmail.com>
2024-09-22 14:49:46 -0300

    Delete line

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
 
 export default function HeroSection({ openModal }) {
     return (
@@ -8,56 +8,46 @@ export default function HeroSection({ openModal }) {
                 initial={{ opacity: 0, x: -50 }}
                 animate={{ opacity: 1, x: 0 }}
                 transition={{ duration: 0.8 }}
-                className="lg:w-1/2 text-left z-10 pr-8"
-            >
+                className="lg:w-1/2 text-left z-10 pr-8">
                 <motion.h1
                     className="text-3xl tracking-tight font-extrabold text-gray-600 sm:text-5xl md:text-6xl leading-relaxed tracking-wide"
                     initial={{ opacity: 0, y: 20 }}
                     animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.7 }}
-                >
-                    <span className="block xl:inline">Revoluciona la Gestión de tu Restaurante con</span>{' '}
+                    transition={{ duration: 0.7 }}>
+                    <span className="block xl:inline">
+                        Revoluciona la Gestión de tu Restaurante con
+                    </span>{" "}
                     <br />
                     <span className="block text-primary xl:inline mt-2">
                         Tu Resto
                     </span>
                 </motion.h1>
 
-                <motion.div
-                    initial={{ width: 0 }}
-                    animate={{ width: "32%" }}
-                    transition={{ duration: 1, delay: 0.5 }}
-                    className="h-1 bg-gray-500 mt-2"
-                />
-
                 <motion.p
                     className="mt-6 text-base text-gray-500 sm:text-lg md:text-xl lg:max-w-full lg:mx-0"
                     initial={{ opacity: 0, y: 20 }}
                     animate={{ opacity: 1, y: 0 }}
-                    transition={{ delay: 0.3, duration: 0.8 }}
-                >
-                    Centraliza tus operaciones, optimiza tus recursos y eleva la experiencia de tus clientes con nuestra plataforma integral.
+                    transition={{ delay: 0.3, duration: 0.8 }}>
+                    Centraliza tus operaciones, optimiza tus recursos y eleva la
+                    experiencia de tus clientes con nuestra plataforma integral.
                 </motion.p>
 
                 <motion.div
                     className="mt-8 sm:mt-10 flex items-center"
                     initial={{ opacity: 0, y: 20 }}
                     animate={{ opacity: 1, y: 0 }}
-                    transition={{ delay: 0.5, duration: 0.8 }}
-                >
+                    transition={{ delay: 0.5, duration: 0.8 }}>
                     <motion.button
                         onClick={openModal}
                         className="bg-primary text-white text-lg px-8 py-3 rounded-full shadow-lg hover:bg-primaryDark transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-opacity-50"
                         whileHover={{ scale: 1.05 }}
-                        whileTap={{ scale: 0.95 }}
-                    >
+                        whileTap={{ scale: 0.95 }}>
                         Comenzar Ahora
                     </motion.button>
                     <motion.a
                         href="#funcionalidades"
                         className="ml-4 text-primary font-semibold hover:text-primaryDark transition duration-300"
-                        whileHover={{ x: 5 }}
-                    >
+                        whileHover={{ x: 5 }}>
                         Descubre más →
                     </motion.a>
                 </motion.div>
@@ -66,9 +56,8 @@ export default function HeroSection({ openModal }) {
                 className="mt-10 lg:mt-0 lg:w-1/2 flex justify-center lg:justify-end relative z-10"
                 initial={{ opacity: 0, x: 50 }}
                 animate={{ opacity: 1, x: 0 }}
-                transition={{ duration: 0.8 }}
-            >
-                <div className="w-full h-auto overflow-hidden rounded-lg shadow-xl">
+                transition={{ duration: 0.8 }}>
+                <div className="w-full h-auto overflow-hidden rounded-lg ">
                     <img
                         src="/jpg/turesto.png"
                         alt="Tu Resto en acción"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
-import { useState } from 'react';
-import Navbar from '../components/Navbar';
-import HeroSection from '../components/HeroSection';
+import { useState } from "react";
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
 import LoginModal from "../components/LoginModal";
 import { useRouter } from "next/router";
-import Subscription from '../components/subscription';
-import Cookies from 'js-cookie';
-import { useEffect } from 'react';
+import Subscription from "../components/subscription";
+import Cookies from "js-cookie";
+import { useEffect } from "react";
 import config from "/utils/config";
-import React from 'react';
-import { FaMobileAlt, FaTabletAlt, FaDesktop } from 'react-icons/fa';
+import React from "react";
+import { FaMobileAlt, FaTabletAlt, FaDesktop } from "react-icons/fa";
 
 export default function Home({ token = true }) {
     const router = useRouter();
@@ -23,34 +23,36 @@ export default function Home({ token = true }) {
 
     useEffect(() => {
         const checkAuthAndRedirect = async () => {
-            const accessToken = Cookies.get('accessToken');
-            const userRole = Cookies.get('user_role');
+            const accessToken = Cookies.get("accessToken");
+            const userRole = Cookies.get("user_role");
 
             if (accessToken) {
                 try {
-                    const response = await fetch(`${config.apiUrl}/user/profile`, {
-                        method: 'GET',
-                        headers: {
-                            'Authorization': `Bearer ${accessToken}`,
-                            'Content-Type': 'application/json'
+                    const response = await fetch(
+                        `${config.apiUrl}/user/profile`,
+                        {
+                            method: "GET",
+                            headers: {
+                                Authorization: `Bearer ${accessToken}`,
+                                "Content-Type": "application/json",
+                            },
                         }
-                    });
+                    );
 
                     if (response.ok && userRole) {
                         router.replace(`/${userRole.toLowerCase()}/dashboard`);
                     } else {
-                        Cookies.remove('accessToken');
-                        Cookies.remove('refreshToken');
-                        Cookies.remove('user_role');
+                        Cookies.remove("accessToken");
+                        Cookies.remove("refreshToken");
+                        Cookies.remove("user_role");
                     }
                 } catch (error) {
-                    console.error('Error al verificar el token:', error);
-                    Cookies.remove('accessToken');
-                    Cookies.remove('refreshToken');
-                    Cookies.remove('user_role');
+                    console.error("Error al verificar el token:", error);
+                    Cookies.remove("accessToken");
+                    Cookies.remove("refreshToken");
+                    Cookies.remove("user_role");
                 }
             }
-            // Si no hay token, el usuario se queda en la página de inicio
         };
 
         checkAuthAndRedirect();
@@ -79,33 +81,48 @@ export default function Home({ token = true }) {
 
                     {/* Sección de Funcionalidades */}
                     <section id="funcionalidades" className="py-20 px-8">
-                        <h2 className="text-4xl font-bold text-center mb-12 text-gray-800">Funcionalidades</h2>
+                        <h2 className="text-4xl font-bold text-center mb-12 text-gray-800">
+                            Funcionalidades
+                        </h2>
                         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
                             {[
                                 {
-                                    title: 'Gestión de Reservas',
-                                    description: 'Optimiza la organización de tus mesas y clientes.',
-                                    icon: 'fas fa-calendar-check'
+                                    title: "Gestión de Reservas",
+                                    description:
+                                        "Optimiza la organización de tus mesas y clientes.",
+                                    icon: "fas fa-calendar-check",
                                 },
                                 {
-                                    title: 'Control de Inventario',
-                                    description: 'Mantén un registro preciso de tus suministros.',
-                                    icon: 'fas fa-boxes'
+                                    title: "Control de Inventario",
+                                    description:
+                                        "Mantén un registro preciso de tus suministros.",
+                                    icon: "fas fa-boxes",
                                 },
                                 {
-                                    title: 'Análisis de Datos',
-                                    description: 'Toma decisiones informadas con estadísticas detalladas.',
-                                    icon: 'fas fa-chart-line'
-                                }
+                                    title: "Análisis de Datos",
+                                    description:
+                                        "Toma decisiones informadas con estadísticas detalladas.",
+                                    icon: "fas fa-chart-line",
+                                },
                             ].map((feature, index) => (
-                                <div key={index} className="bg-white p-6 rounded-lg shadow-lg overflow-hidden relative group">
+                                <div
+                                    key={index}
+                                    className="bg-white p-6 rounded-lg shadow-lg overflow-hidden relative group">
                                     <div className="transition-all duration-300 ease-in-out transform group-hover:-translate-y-full">
-                                        <i className={`${feature.icon} text-4xl text-indigo-600 mb-4`}></i>
-                                        <h3 className="text-xl font-semibold mb-4 text-indigo-600">{feature.title}</h3>
-                                        <p className="text-gray-600">{feature.description}</p>
+                                        <i
+                                            className={`${feature.icon} text-4xl text-indigo-600 mb-4`}></i>
+                                        <h3 className="text-xl font-semibold mb-4 text-indigo-600">
+                                            {feature.title}
+                                        </h3>
+                                        <p className="text-gray-600">
+                                            {feature.description}
+                                        </p>
                                     </div>
                                     <div className="absolute inset-0 bg-indigo-600 text-white p-6 transition-all duration-300 ease-in-out transform translate-y-full group-hover:translate-y-0 flex items-center justify-center text-center">
-                                        <p className="text-lg">Descubre cómo esta funcionalidad puede revolucionar tu negocio</p>
+                                        <p className="text-lg">
+                                            Descubre cómo esta funcionalidad
+                                            puede revolucionar tu negocio
+                                        </p>
                                     </div>
                                 </div>
                             ))}
@@ -119,25 +136,43 @@ export default function Home({ token = true }) {
                         </h2>
                         <div className="flex flex-wrap justify-center gap-16">
                             {[
-                                { name: 'Móvil', icon: FaMobileAlt, description: 'Accede a tu restaurante desde cualquier lugar con nuestra app móvil.' },
-                                { name: 'Tablet', icon: FaTabletAlt, description: 'Perfecta para tomar pedidos en mesa o gestionar el inventario.' },
-                                { name: 'Escritorio', icon: FaDesktop, description: 'Gestiona tu negocio con todas las funcionalidades en tu ordenador.' }
+                                {
+                                    name: "Móvil",
+                                    icon: FaMobileAlt,
+                                    description:
+                                        "Accede a tu restaurante desde cualquier lugar con nuestra app móvil.",
+                                },
+                                {
+                                    name: "Tablet",
+                                    icon: FaTabletAlt,
+                                    description:
+                                        "Perfecta para tomar pedidos en mesa o gestionar el inventario.",
+                                },
+                                {
+                                    name: "Escritorio",
+                                    icon: FaDesktop,
+                                    description:
+                                        "Gestiona tu negocio con todas las funcionalidades en tu ordenador.",
+                                },
                             ].map((device, index) => (
                                 <div key={index} className="relative group">
                                     <div className="text-center transition-all duration-300 ease-in-out cursor-pointer">
                                         <div className="w-40 h-40 mx-auto mb-8 bg-gradient-to-br from-indigo-500 to-purple-600 rounded-3xl shadow-xl flex items-center justify-center animate-float group-hover:shadow-2xl">
                                             <device.icon className="text-5xl text-white" />
                                         </div>
-                                        <p className="text-2xl font-semibold text-gray-800">{device.name}</p>
+                                        <p className="text-2xl font-semibold text-gray-800">
+                                            {device.name}
+                                        </p>
                                     </div>
                                     <div className="absolute inset-0 bg-white rounded-3xl shadow-2xl opacity-0 group-hover:opacity-100 transition-all duration-300 ease-in-out z-10 flex items-center justify-center">
-                                        <p className="text-gray-700 text-center px-4">{device.description}</p>
+                                        <p className="text-gray-700 text-center px-4">
+                                            {device.description}
+                                        </p>
                                     </div>
                                 </div>
                             ))}
                         </div>
                     </section>
-
                 </div>
             </div>
             <LoginModal isOpen={isModalOpen} closeModal={closeModal} />
